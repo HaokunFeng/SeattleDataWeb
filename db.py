d2d123997c125f440dd0e941869372e205134e11
Haokun.Feng <haokunf@uw.edu>
2024-02-08 13:11:48 -0800

    update

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ load_dotenv()
 db_user = os.getenv('DB_USER')
 db_pw = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
-db_port = 5432
+db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 conn_str = f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}'
 
